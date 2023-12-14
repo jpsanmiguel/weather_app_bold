7895430b9e62301ed2531de4013443d9272006ff
Sanmi <juanpasanmi@gmail.com>
2023-12-14 17:39:33 -0500

    Added load of images to WeatherLocationDayViewHolder.

--- app/src/main/java/com/sanmi/labs/weatherapp/presentation/weather_location/adapter/view_holder/WeatherLocationDayViewHolder.kt
+++ app/src/main/java/com/sanmi/labs/weatherapp/presentation/weather_location/adapter/view_holder/WeatherLocationDayViewHolder.kt
@@ -3,6 +3,7 @@ package com.sanmi.labs.weatherapp.presentation.weather_location.adapter.view_hol
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.sanmi.labs.bold.weather_app.databinding.ItemWeatherLocationDayBinding
 import com.sanmi.labs.weatherapp.presentation.model.WeatherLocationDayItem
 
@@ -13,6 +14,13 @@ class WeatherLocationDayViewHolder private constructor(
     fun bind(weatherLocationDayItem: WeatherLocationDayItem, locationFullName: String) {
         binding.weatherLocationDayItem = weatherLocationDayItem
         binding.locationFullName = locationFullName
+
+        Glide
+            .with(binding.root)
+            .load("https:${weatherLocationDayItem.conditionImage}")
+            .centerCrop()
+//            .placeholder(R.drawable.loading_spinner)
+            .into(binding.forecastDayCurrentConditionImage)
     }
 
     companion object {
