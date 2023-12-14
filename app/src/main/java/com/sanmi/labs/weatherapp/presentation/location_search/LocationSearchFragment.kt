404c866f6fde4fc92b3502957d34b2e3ade176e4
Sanmi <juanpasanmi@gmail.com>
2023-12-14 13:17:50 -0500

    Added logic to call service of search while the user types.

--- app/src/main/java/com/sanmi/labs/weatherapp/presentation/location_search/LocationSearchFragment.kt
+++ app/src/main/java/com/sanmi/labs/weatherapp/presentation/location_search/LocationSearchFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.core.view.isVisible
+import androidx.core.widget.doOnTextChanged
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import com.sanmi.labs.bold.weather_app.databinding.FragmentLocationSearchBinding
@@ -31,6 +32,10 @@ class LocationSearchFragment : Fragment() {
         binding.viewModel = locationSearchViewModel
         binding.lifecycleOwner = viewLifecycleOwner
 
+        binding.searchTextInputText.doOnTextChanged { text, _, _, _ ->
+            text?.let { locationSearchViewModel.onSearchTextChange(text.toString()) }
+        }
+
         locationAdapter = LocationAdapter(LocationAdapter.OnClickListener {
             findNavController().navigate(
                 LocationSearchFragmentDirections.actionLocationSearchFragmentToWeatherLocationFragment(
@@ -38,6 +43,7 @@ class LocationSearchFragment : Fragment() {
                 )
             )
         })
+
         binding.locationRecyclerView.apply {
             adapter = locationAdapter
         }
--- app/src/main/java/com/sanmi/labs/weatherapp/presentation/location_search/LocationSearchViewModel.kt
+++ app/src/main/java/com/sanmi/labs/weatherapp/presentation/location_search/LocationSearchViewModel.kt
@@ -9,6 +9,9 @@ import com.sanmi.labs.weatherapp.domain.model.Location
 import com.sanmi.labs.weatherapp.domain.use_case.SearchUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.Job
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -20,14 +23,24 @@ class LocationSearchViewModel @Inject constructor(
     private var searchWeatherJob: Job? = null
 
     private val _locations = MutableLiveData<List<Location>>()
+
+    private val searchText = MutableStateFlow("")
     val locations: LiveData<List<Location>>
         get() = _locations
 
     init {
-        searchWeather("Bogo")
+        listenToTextChanges()
     }
 
-    fun searchWeather(query: String) {
+    private fun listenToTextChanges() {
+        searchText.onEach { searchWeather(it) }.launchIn(viewModelScope)
+    }
+
+    fun onSearchTextChange(searchText: String) {
+        this.searchText.value = searchText
+    }
+
+    private fun searchWeather(query: String) {
         searchWeatherJob?.cancel()
         _status.value = Status.Loading
         searchWeatherJob = viewModelScope.launch {
