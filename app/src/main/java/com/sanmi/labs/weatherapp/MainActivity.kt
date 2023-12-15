a99ce20d5cc77abc3501b34ad3320e480f0b2627
Sanmi <juanpasanmi@gmail.com>
2023-12-14 22:20:50 -0500

    Added icon and splash to application.

--- app/src/main/java/com/sanmi/labs/weatherapp/MainActivity.kt
+++ app/src/main/java/com/sanmi/labs/weatherapp/MainActivity.kt
@@ -2,6 +2,7 @@ package com.sanmi.labs.weatherapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.sanmi.labs.bold.weather_app.R
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -9,6 +10,8 @@ import dagger.hilt.android.AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
